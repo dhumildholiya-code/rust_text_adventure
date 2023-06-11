78780558abaae02107c5586408956b8171f50047
Dhumil Dholiya <dhumildholiya97@gmail.com>
2023-06-11 14:58:14 +0530

    refactored some function names

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 mod recursive_parser;
 
-use recursive_parser::{parse, Vocabulary};
+use recursive_parser::{parse_input, Vocabulary};
 
 fn main() {
     let vocabulary: Vocabulary = Vocabulary::new();
     let input = String::from("go moon rusty sword");
 
-    let parseRes = parse(input, &vocabulary);
+    let parseRes = parse_input(input, &vocabulary);
 }
--- src/recursive_parser/mod.rs
+++ src/recursive_parser/mod.rs
@@ -7,26 +7,26 @@ pub use vocab::Vocabulary;
 //use for inner modules.
 use parser::{Token, Tokenizer};
 
-pub fn parse(source: String, vocab: &Vocabulary) {
+pub fn parse_input(source: String, vocab: &Vocabulary) {
     let mut tokens = Tokenizer::create_tokens(source, vocab);
-    sentence(&mut tokens);
+    sentence_expr(&mut tokens);
     match tokens.get_token() {
         Token::Eof => println!("Parsed Successfully!"),
         _ => (),
     }
 }
 
-fn sentence(tokens: &mut Tokenizer) {
+fn sentence_expr(tokens: &mut Tokenizer) {
     let token = tokens.get_token();
     match token {
         Token::Verb(_) => {
             tokens.next_token();
-            object(tokens);
+            object_expr(tokens);
         }
         _ => return,
     }
 }
-fn object(tokens: &mut Tokenizer) {
+fn object_expr(tokens: &mut Tokenizer) {
     let mut token = tokens.get_token();
     match token {
         Token::Adjective(_) => tokens.next_token(),
