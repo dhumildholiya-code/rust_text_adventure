7e44e680b935eba56236a0bbe0fe0cd7a4ffb118
Dhumil Dholiya <dhumildholiya97@gmail.com>
2023-06-13 08:43:55 +0530

    added getter for item description.
    - added unit test for it also.

--- src/engine/item.rs
+++ src/engine/item.rs
@@ -16,6 +16,9 @@ impl Item {
     pub fn get_room_position(&self) -> String {
         self.room_position.clone()
     }
+    pub fn get_description(&self) -> String {
+        self.description.clone()
+    }
 }
 
 #[cfg(test)]
@@ -27,5 +30,11 @@ mod tests {
         let item = Item::new(0, "item", "{name} lying on ground.", "desc");
         let expected = "item lying on ground.";
         assert_eq!(item.get_room_position(), expected);
+    }
+    #[test]
+    fn testing_item_description() {
+        let item = Item::new(0, "item", "{name} lying", "{name} is a god key.");
+        let expected = "item is a god key.";
+        assert_eq!(item.get_description(), expected);
     }
 }
